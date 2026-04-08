234c31dd7b87c8258543e14d6c08b4560b12e2d0
Louis Avisse <louis.avisse@outlook.fr>
2026-04-08 12:09:54 +0200

    UI upgrades

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // Layer Numbering Plugin - Main Logic
 
-figma.showUI(__html__, { width: 280, height: 340 });
+figma.showUI(__html__, { width: 260, height: 300 });
 
 figma.ui.onmessage = (msg) => {
   if (msg.type === 'number-layers') {
